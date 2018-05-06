7eeb3a095f50cb899ee468bece63f351d80a45de
Hayden Matthews <haydenamatthews@yahoo.com>
2018-05-06 09:58:15 -0600

    Added second Arm

---
+++ Wave-Race/Assets/AddPoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPoints : MonoBehaviour {
+
+    private int points;
+    public GameObject partner;
+
+	// Use this for initialization
+	void Start () {
+        points = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Arm"))
+        {
+            Debug.Log("+1 point");
+            points += 1;
+            gameObject.SetActive(false);
+            partner.SetActive(true);
+        }
+    }
+}
--- Wave-Race/Assets/Scripts/Movement.cs
+++ Wave-Race/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public GameObject arm;
+    public string PlayerNumber;
 
 	// Use this for initialization
 	void Start () {
@@ -13,12 +14,27 @@ public class Movement : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.LeftArrow)) { 
-		    arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.forward, 5, Space.Self);
+        if (PlayerNumber.Equals("1"))
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.forward, 5, Space.Self);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.back, 5, Space.Self);
+            } 
         }
-	    if (Input.GetKey(KeyCode.RightArrow))
-	    {
-	        arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.back, 5, Space.Self);
-	    }
+        if (PlayerNumber.Equals("2"))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.forward, 5, Space.Self);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                arm.GetComponent<Rigidbody>().transform.Rotate(Vector3.back, 5, Space.Self);
+            }
+        }
     }
 }
